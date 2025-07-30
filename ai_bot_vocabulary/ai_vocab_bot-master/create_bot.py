c3945a00e2ae9395baba24e512b455a4621248ec
vladiemir1 <114985274+vladiemir1@users.noreply.github.com>
2025-07-30 16:55:28 +0400

    Delete ai_bot_vocabulary directory

--- ai_bot_vocabulary/ai_vocab_bot-master/create_bot.py
+++
@@ -1,6 +0,0 @@
-import os
-from aiogram import Bot
-from aiogram import Dispatcher
-
-bot: Bot = Bot(token=os.environ['TOKEN_VOCAB'], parse_mode='HTML')
-dp: Dispatcher = Dispatcher()
--- ai_bot_vocabulary/ai_vocab_bot-master/word2vec_model.py
+++
@@ -1,6 +0,0 @@
-from gensim.models import KeyedVectors
-
-# Загружаем сохраненную модель с диска
-model_word2vec = KeyedVectors.load('glove-wiki-gigaword-100.model', mmap='r')
-
-print(model_word2vec)
